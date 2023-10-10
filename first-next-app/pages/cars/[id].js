2378b5c92b1b95f2563f557dfe2cd6a6a74460c2
Nidheesh PV <nidheesh@vonnue.com>
2023-10-10 18:46:40 +0530

    feat: implement static rendering with dynamic id

--- first-next-app/pages/cars/[id].js
+++ first-next-app/pages/cars/[id].js
@@ -1,8 +1,43 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
-export default function Cars() {
+export default function Cars({ car }) {
     const router = useRouter();
     const { id } = router.query;
 
-    return <h1>hello {id} </h1>
+    return (
+        <div>
+            <Head>
+                <title>{car.color} {car.id}</title>
+            </Head>
+            <h1>hello {id} </h1>
+            <p>{car.color}</p>
+            <img src={car.image} />
+        </div>
+    )
+}
+
+export async function getStaticPaths() {
+    const res = await fetch('http://localhost:3000/cars.json')
+    const cars = await res.json()
+
+    const paths = cars.map((car) => ({
+        params: { id: car.id.toString() },
+    }))
+
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { car: data }
+    }
+
 }
