2d1cc7402618feef407ee07b7a80af568494a97c
Nidheesh PV <nidheesh@vonnue.com>
2023-10-18 10:13:28 +0530

    feat: add pages and components to strapi

--- next-strapi-graphql-site/backend/types/generated/components.d.ts
+++ next-strapi-graphql-site/backend/types/generated/components.d.ts
@@ -1,54 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface ComponentsCategoryList extends Schema.Component {
-  collectionName: 'components_components_category_lists';
-  info: {
-    displayName: 'CategoryList';
-    icon: 'bulletList';
-  };
-  attributes: {
-    categories: Attribute.Relation<
-      'components.category-list',
-      'oneToMany',
-      'api::category.category'
-    >;
-  };
-}
-
-export interface ComponentsNavbar extends Schema.Component {
-  collectionName: 'components_components_navbars';
-  info: {
-    displayName: 'Navbar';
-    icon: 'filter';
-    description: '';
-  };
-  attributes: {
-    backLabel: Attribute.String;
-    backURL: Attribute.String;
-    pageTitle: Attribute.String;
-    ItemsLabel: Attribute.String;
-  };
-}
-
-export interface ComponentsSelletItem extends Schema.Component {
-  collectionName: 'components_components_sellet_items';
-  info: {
-    displayName: 'SelletItem';
-    icon: 'user';
-  };
-  attributes: {
-    closeIcon: Attribute.Media;
-    phoneLabel: Attribute.String;
-    detailsHeader: Attribute.String;
-  };
-}
-
 declare module '@strapi/types' {
-  export module Shared {
-    export interface Components {
-      'components.category-list': ComponentsCategoryList;
-      'components.navbar': ComponentsNavbar;
-      'components.sellet-item': ComponentsSelletItem;
-    }
-  }
+  export module Shared {}
 }
